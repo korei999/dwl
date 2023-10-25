c360d5d533c54639655d0e328f651d02d910bcd9
korei999 <ju7t1xe@gmail.com>
2023-10-25 14:40:30 +0300

    add pointer constraints

--- config.def.h
+++ config.def.h
@@ -31,6 +31,9 @@ static const char  kblayout_file[] = "/tmp/dwl-keymap";
 static const char *kblayout_cmd[]  = {"pkill", "-RTMIN+1", "someblocks", NULL};
 static const int   kblayout_perclient = 0;
 
+/* pointer constraints */
+static const int allow_constrain      = 1;
+
 static const Rule rules[] = {
 	/* app_id     title       tags mask     isfloating   monitor */
 	/* examples:
